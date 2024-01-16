e9519fe33b3621d033dee1bba26da22ddc341cb3
633jinn <judyg00dy68@gmail.com>
2024-01-16 23:49:14 +0900

    feat: 기본적인 spring setting

---
+++ src/test/java/com/letsRoll/letsRoll_New/LetsRollNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letsRoll.letsRoll_New;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsRollNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
